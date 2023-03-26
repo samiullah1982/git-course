6a8bef0094107765f3a94f5559de38721534411e
Sami Ullah <Samipiac@gmail.com>
2023-03-27 00:44:27 +0500

    Header.JS updated

--- header.js
+++ header.js
@@ -8,4 +8,7 @@ i am creating a second conflict and its local change
  i am doing second conflict on remote server
 
 
+******************i am new added to check if the git is updating remote server:wq
+
+
 dgethis was remote
